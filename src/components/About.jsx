d9e27670e540462bee94d5f48f5ad73ed8bdf497
Mrugapatel <107723608+Mrugapatel1007@users.noreply.github.com>
2022-10-09 17:55:41 -0500

    changes

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const About = props => {
+
+    const Wrapper = styled.div`
+        min-height: 94vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    `
+
+    return(
+        <Wrapper>
+            <h1>Gazal Patel</h1>
+            <p>HI, I'M A SOFTWARE DEVELOPER IN Canada</p>
+        </Wrapper>
+    )
+}
+
+export default About;
